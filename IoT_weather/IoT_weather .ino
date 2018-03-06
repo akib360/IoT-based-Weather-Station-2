4b03a72b686428f16577e93627195e77d16405b8
akib hosen khan <36074470+akib360@users.noreply.github.com>
2018-03-06 23:25:12 +0600

    Add files via upload

---
+++ IoT_weather/IoT_weather .ino
@@ -0,0 +1,68 @@
+
+
+#include <DHT.h>
+#include <Adafruit_Sensor.h>
+#include <ESP8266WiFi.h>
+
+#define DHTPIN D5
+#define DHTTYPE  DHT11
+
+const char* ssid        = "your wifi ssid";
+const char* password    = "your password";
+const char* host        = "api.thingspeak.com";
+const char* writeAPIKey = "your api";
+
+DHT dht(DHTPIN, DHTTYPE, 15);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+  delay(1000);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println(".");
+  }
+}
+
+void loop() {
+ float humidity = dht.readHumidity();
+ float temperature = dht.readTemperature();
+
+  if (isnan(humidity) || isnan(temperature)) {
+    return;
+  }
+
+// make TCP connections
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    return;
+  }
+
+  String url = "/update?key=";
+  url+=writeAPIKey;
+  url+="&field1=";
+  url+=String(temperature);
+  url+="&field2=";
+  url+=String(humidity);
+  url+="\r\n";
+
+  // Request to the server
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "Connection: close\r\n\r\n");
+
+    
+    Serial.print("Temperature Value is :");  
+  Serial.print(temperature);  
+  Serial.println("C");  
+  Serial.print("Humidity Value is :");  
+  Serial.print(humidity);  
+  Serial.println("%");  
+    
+    
+    
+    delay(5000);
+}
+
